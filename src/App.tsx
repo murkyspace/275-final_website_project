afa4aecb9a29117a6dc5dbf2bc7325fe5e255a50
Connor <ccchip@udel.edu>
2024-10-08 16:14:23 -0400

    Added my name

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>Ray Fischer</p>
+        <p>Connor Chipoletti</p>
         <a
           className="App-link"
           href="https://reactjs.org"
