402cca867dd671706043e96fadccf51eea1e257e
Ray <lunemurk@udel.edu>
2024-10-08 12:58:12 -0400

    testing new clone repository

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>Ray Fischer.</p>
+        <p>Ray Fischer</p>
         <a
           className="App-link"
           href="https://reactjs.org"
