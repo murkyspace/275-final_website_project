3f709ce820770b0172b489fe54d679096a74129b
Connor <ccchip@udel.edu>
2024-10-16 14:27:01 -0400

    progress towards navigation

--- src/Basic.tsx
+++ src/Basic.tsx
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import App from './App';
 import './Basic.css';
 import { Button, Form } from 'react-bootstrap';
 
 
-  
 
   
 
 function Basic() {
-    return (
-        <div className="Basic">
-          <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Basic />} />
-            </Routes>
-            </BrowserRouter>
-        </div>
-      );
+  const navigate = useNavigate();
+
+  const goToApp = () => {
+    // This will navigate to first component
+    navigate('/App');
+  };
+
+
+  return (
+    <div className="Basic">
+      <Button onClick={goToApp}>Click me!</Button>
+    </div>
+  );
 }
 
 export default Basic;
