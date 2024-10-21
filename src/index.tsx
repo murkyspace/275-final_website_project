734d555cb451c17a2d570ac0dd68cff00e23b680
ZhihuaSun1 <sunzhi@udel.edu>
2024-10-21 14:18:07 -0400

    QuizPage

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Main from './Main';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+      <Main />
+    </Router>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
