06d9d27ae346e513e86ef0fbed51997abb1bb28e
ZhihuaSun1 <sunzhi@udel.edu>
2024-11-13 20:09:09 -0500

    IMPORVE It makes our app cleaner and simpler and completes the detailed landscaping and changes the UI to make it really nice and easy to use.

--- src/DetailedInt.ts
+++ src/DetailedInt.ts
@@ -1,6 +1,5 @@
 export interface DetailedInterface {
-    setCurrPage: React.Dispatch<React.SetStateAction<number>>;
-    setApiResponse: React.Dispatch<React.SetStateAction<string>>;
-  }
-export {}
+  setCurrPage: (page: number) => void;
+  setApiResponse: (response: string) => void;
+}
 
